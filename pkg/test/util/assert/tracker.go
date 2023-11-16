9311e7cf30ac74f6eb0c1e23f85f65d26ab9c45f
John Howard <howardjohn@google.com>
2023-11-16 10:34:33 -0800

    Initial krt library

--- pkg/test/util/assert/tracker.go
+++ pkg/test/util/assert/tracker.go
@@ -19,6 +19,7 @@ import (
 	"sync"
 	"time"
 
+	"istio.io/istio/pkg/ptr"
 	"istio.io/istio/pkg/test"
 	"istio.io/istio/pkg/test/util/retry"
 )
@@ -69,5 +70,56 @@ func (t *Tracker[T]) WaitOrdered(events ...T) {
 			return nil
 		}, retry.Timeout(time.Second))
 	}
+	t.Empty()
+}
+
+// WaitUnordered waits for an event to happen, in any order
+func (t *Tracker[T]) WaitUnordered(events ...T) {
+	t.t.Helper()
+	want := map[T]struct{}{}
+	for _, e := range events {
+		want[e] = struct{}{}
+	}
+	retry.UntilSuccessOrFail(t.t, func() error {
+		t.mu.Lock()
+		defer t.mu.Unlock()
+		if len(t.events) == 0 {
+			return fmt.Errorf("no events")
+		}
+		got := t.events[0]
+		if _, f := want[got]; !f {
+			t.t.Fatalf("got events %v, want %v", t.events, want)
+		}
+		// clear the event
+		t.events[0] = ptr.Empty[T]()
+		t.events = t.events[1:]
+		delete(want, got)
+
+		if len(want) > 0 {
+			return fmt.Errorf("still waiting for %v", want)
+		}
+		return nil
+	}, retry.Timeout(time.Second))
+	t.Empty()
+}
+
+// WaitCompare waits for an event to happen and ensures it meets a custom comparison function
+func (t *Tracker[T]) WaitCompare(f func(T) bool) {
+	t.t.Helper()
+	retry.UntilSuccessOrFail(t.t, func() error {
+		t.mu.Lock()
+		defer t.mu.Unlock()
+		if len(t.events) == 0 {
+			return fmt.Errorf("no events")
+		}
+		got := t.events[0]
+		if !f(got) {
+			t.t.Fatalf("got events %v, which does not match criteria", t.events)
+		}
+		// clear the event
+		t.events[0] = ptr.Empty[T]()
+		t.events = t.events[1:]
+		return nil
+	}, retry.Timeout(time.Second))
 	t.Empty()
 }
